b350f8eb291c3a009fe044e35e992af73e7eee7e
fahim2000-f <mohammedaminefahim567@gmail.com>
2022-08-08 18:46:05 +0100

    remove images

--- src/App.js
+++ src/App.js
@@ -1,66 +1,59 @@
 import React, { useEffect, useState } from "react";
-import './App.css'
+import "./App.css";
 import MovieCards from "./MovieCards";
-import searchicon from './search.svg'
-
+import searchicon from "./search.svg";
 
 const api_url = "https://www.omdbapi.com?apikey=6cfb7c5";
 
-
-
 function App() {
+  const [movies, setmovies] = useState([]);
+  const [searchterm, setsearchterm] = useState();
 
-    const [movies,setmovies]=useState([])
-    const [searchterm,setsearchterm]=useState()
+  const searshmovie = async (title) => {
+    const response = await fetch(`${api_url}&s=${title}`);
+    const data = await response.json();
+    setmovies(data.Search);
+  };
 
-  const searshmovie = async(title)=>{
-    const response = await fetch(`${api_url}&s=${title}`)
-    const data = await response.json()
-    setmovies(data.Search)
-  }
-
-  useEffect(()=>{
-    searshmovie('spiderman')
-  },[]);
-
+  useEffect(() => {
+    searshmovie("spiderman");
+  }, []);
 
   return (
-  <div className="app">
-    <h1>MovieLand</h1>
+    <div className="app">
+      <h1>MovieLand</h1>
 
-    <div className="search">
-      
-      <input 
-      placeholder="search for movie"
-      value={searchterm}
-      onChange={(e)=>{setsearchterm(e.target.value)}}
-      />
-      
-      <img 
-      src={searchicon}
-      alt="search"
-      onClick ={()=>{searshmovie(searchterm)}}
-      />
-    </div>
+      <div className="search">
+        <input
+          placeholder="search for movie"
+          value={searchterm}
+          onChange={(e) => {
+            setsearchterm(e.target.value);
+          }}
+        />
 
-{
-      movies?.length >0 ? (
-        <div className="container">
-        {movies.map((movie)=>(
-          <MovieCards movie={movie}/>
-        ))}
-        
+        <img
+          src={searchicon}
+          alt="search"
+          onClick={() => {
+            searshmovie(searchterm);
+          }}
+        />
       </div>
-      )
-      :(
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCards movie={movie} />
+          ))}
+        </div>
+      ) : (
         <div className="empty">
           <h2>No movies found</h2>
         </div>
-      )
-}
-   
-  </div>
-  )
+      )}
+    </div>
+  );
 }
 
 export default App;
